f0c48a387f050eb20655f72412b489bc0fa33975
konstantin <k.korobanov@info.grfc.ru>
2020-05-11 21:45:24 +0300

    Added logging db connection exceptions

--- nous_auth/db/base.py
+++ nous_auth/db/base.py
@@ -1,15 +1,25 @@
 import asyncpg
+import logging
+import traceback
 from contextlib import asynccontextmanager
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def db_connect(config: dict):
     """Обрабатывает подключение к базе данных."""
-    connect = await asyncpg.connect(**config)
-
     try:
-        yield connect
+        connect = await asyncpg.connect(**config)
+
+        try:
+            yield connect
+        except Exception as err:
+            logger.warning(f'DB CONNECTION EXCEPTION: {err}.')
+            pass
+        finally:
+            await connect.close()
+
     except Exception as err:
-        pass
-    finally:
-        await connect.close()
+        logger.error(f'DB CONNECTION ERROR: {traceback.print_exc()}.')
+        raise err
