06f356f886a9a3694a8d2c6f25e2283486895c5f
konstantin <k.korobanov@info.grfc.ru>
2020-05-21 17:32:48 +0300

    Added user authentication view

--- nous_auth/view/user.py
+++ nous_auth/view/user.py
@@ -1,11 +1,68 @@
 from aiohttp import web, ClientSession
 
-from nous_auth.query.user import rm_token
+from nous_auth.query.user import rm_token, get_token, get_user
+from nous_auth.schema.user import GetAccount
 
 
 async def user_authentication(request):
-    """Check token and return user data."""
-    return web.Response(text='authentication')
+    """
+    ---
+    description: Check token and return user data.
+    tags:
+    - Get user data by token.
+    responses:
+        "200":
+            description: Token is active and return user data.
+            schema:
+                type: object
+                properties:
+                    email:
+                        type:
+                            - "string"
+                            - "null"
+                        description: user email, need for user authorization.
+                    username:
+                        type:
+                            - "string"
+                            - "null"
+                        description: name of user.
+                    phone:
+                        type:
+                            - "int"
+                            - "null"
+                        description: user phone number without county prefix.
+                    create_date:
+                        type: int
+                        description: unixtime date of create user account.
+                    vip:
+                        type: int
+                        description: last end time of vip status.
+                    blocked:
+                        type: int
+                        description: last end time of user block.
+                    role:
+                        type: int
+                        description: id of user role.
+                    country:
+                        type: int
+                        description: user live country phone prefix
+        "204":
+            description: Token not found, need authorization.
+    """
+    token = request.match_info['token']
+    app = request.app
+
+    async with app['auth-db'] as conn:
+        token_obj = await get_token(conn, token)
+
+        # token wasn't exist
+        if not token_obj:
+            return web.HTTPNoContent()
+
+        user_obj = await get_user(conn, token_obj.get('user_id'))
+        user = GetAccount().dump(user_obj)
+
+    return web.json_response(data=user, status=200)
 
 
 async def token_cancel(request):
